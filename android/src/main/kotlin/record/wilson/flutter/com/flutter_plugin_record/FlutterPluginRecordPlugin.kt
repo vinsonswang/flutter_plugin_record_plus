aaf05a3b1fe5f3e24f1803b058fa3471b4ffff05
Rain <25654559+RA1NO3O@users.noreply.github.com>
2025-06-08 18:40:49 +0800

    remove: unused import

--- android/src/main/kotlin/record/wilson/flutter/com/flutter_plugin_record/FlutterPluginRecordPlugin.kt
+++ android/src/main/kotlin/record/wilson/flutter/com/flutter_plugin_record/FlutterPluginRecordPlugin.kt
@@ -21,7 +21,6 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import record.wilson.flutter.com.flutter_plugin_record.utils.*
 import java.io.File
 import java.util.*
